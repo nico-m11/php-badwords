ee0aacb046504c37933bd29128960166c6032e2e
Nicola <nicolamelito.11@gmail.com>
2020-12-22 17:12:32 +0100

    revisionato

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
+  $badWord = $_GET["badword"];
   $mia_variabile = 'Dicette ‘o ciciniello vicino ‘o squal : “pur’io sò pesce”';
-  $modifica = str_replace('pesce', '***', $mia_variabile);
+  $modifica = str_replace($badWord, '***', $mia_variabile);
  ?>
- <h1><?php echo $mia_variabile ?></h1>
+ <!--<h1><?php echo $mia_variabile ?></h1> -->
  <h1><?php echo $modifica ?></h1>
