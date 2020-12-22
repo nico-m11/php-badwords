7ee0cffc1e146b1b0112db9064ff67faa9db62a5
Nicola <nicolamelito.11@gmail.com>
2020-12-22 15:41:58 +0100

    completato

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+  $mia_variabile = 'Dicette ‘o ciciniello vicino ‘o squal : “pur’io sò pesce”';
+  $modifica = str_replace('pesce', '***', $mia_variabile);
+ ?>
+ <h1><?php echo $mia_variabile ?></h1>
+ <h1><?php echo $modifica ?></h1>
